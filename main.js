02e129855e85d8ba36d0553a51b2f06fc4212581
IraMriyka <kovalirinafb@gmail.com>
2022-07-26 23:32:43 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,68 @@
+let n1 = 1;
+let n2 = 2;
+let n3 = 3;
+let n4 = 4;
+let n5 = 5;
+let n6 = 6;
+let n7 = 7;
+let n8 = 8;
+let n9 = 9;
+let n10 = 10;
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers);
+
+let library1 = {
+    title: 'The Martian Chronicles',
+    pageCount: 150,
+    genre: 'fantasy',
+    author: {
+        name: 'Ray Bradbury',
+        age: 1920-2012
+    }
+};
+console.log(library1);
+
+let library2 = {
+    title: 'Gone with the Wind',
+    pageCount: 300,
+    genre: 'romance novel',
+    author: {
+        name: 'Margaret Mitchell',
+        age: 1900-1949
+    }
+};
+console.log(library2);
+
+let library3 = {
+    title: 'Murder on the Orient Express',
+    pageCount: 170,
+    genre: 'detective',
+    author: {
+        name: 'Agatha Christie',
+        age: 1890-1976
+    }
+};
+console.log(library3);
+
+let users = [
+    {name: 'ira', username: 'hfdjj', password: 111},
+    {name: 'anya', username: 'gfeh', password: 123},
+    {name: 'olya', username: 'pfgf', password: 567},
+    {name: 'tanya', username: 'dssfdf', password: 456},
+    {name: 'masha', username: 'gfghf', password: 345},
+    {name: 'roma', username: 'llpl', password: 890},
+    {name: 'ihor', username: 'jfgj', password: 765},
+    {name: 'sasha', username: 'tftf', password: 888},
+    {name: 'katya', username: 'lkjlkh', password: 745},
+    {name: 'andriy', username: 'fdgfd', password: 666},
+];
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
